9008b47104598a82fa7265b998b07f9e4da5b1ec
Chi <cmyip99@gmail.com>
2021-09-07 22:30:58 +1000

    Copied working files into repo

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+const app = express();
+app.listen(8080);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+mongoose.connect('mongodb://localhost:27017/movies', function (err) {
+    if (err) {
+        return console.log('Mongoose - connection error:', err);
+    }
+    console.log('Connected Successfully');
+});
+
+//Actor endpoints 
+app.get('/actors', actors.getAll);
+app.post('/actors', actors.createOne);
+app.get('/actors/:id', actors.getOne);
+app.put('/actors/:id', actors.updateOne);
+app.post('/actors/:id/movies', actors.addMovie);
+app.delete('/actors/:id', actors.deleteOne);
+app.delete('/actors/:id/movies', actors.deleteActorAndMovies);
+
+//Movie endpoints
+app.get('/movies', movies.getAll);
+app.post('/movies', movies.createOne);
+app.post('/movies/:id/actors', movies.addActor);
+app.get('/movies/:id', movies.getOne);
+app.put('/movies/:id', movies.updateOne);
+app.delete('/movies/:id', movies.deleteOne);
